30d4bc41208f8f64aeb5fa6f6c0c45b043560318
Mercadale <emmanuelle.mercadal@new-immo-group.com>
2019-10-15 14:06:39 +0200

    [DM-2689] Suppression images cache

--- include/image_cache.cls.php
+++ include/image_cache.cls.php
@@ -92,6 +92,7 @@ class Image_Cache {
           // Image not found or invalid
           if ( strlen($image) == 0 ) {
             $msg = ($data_uri ? "Data-URI could not be parsed" : "Image not found");
+            unlink($resolved_url);
             throw new DOMPDF_Image_Exception($msg);
           }
           
@@ -152,7 +153,7 @@ class Image_Cache {
    */
   static function clear() {
     if ( empty(self::$_cache) || DEBUGKEEPTEMP ) return;
-    
+
     foreach ( self::$_cache as $file ) {
       if (DEBUGPNG) print "[clear unlink $file]";
       unlink($file);
